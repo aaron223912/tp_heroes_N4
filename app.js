2faf7df339fdc0d01dd8c8ce1092da44cc935e6d
aaron223912 <aaron.m.arias96@gmail.com>
2022-06-20 18:22:23 -0300

    agrege imagenes y archivos html

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const port = 3030;
 
 app.listen(port,() =>console.log('server running in http://localhost:' +port))
 
-app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','index.html')))
+/* RECURSOS ESTATICOS */
+app.use(express.static('public'));
 
+/*RUTAS */
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','index.html')))
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','index.html')))
 app.get('/babbage',(req,res)=>res.sendFile(path.join(__dirname,'views','babbage.html')))
 app.get('/berners-lee',(req,res)=>res.sendFile(path.join(__dirname,'views','berners.html')))
 app.get('/clarke',(req,res)=>res.sendFile(path.join(__dirname,'views','clarke.html')))
-app.get('/hamilton ',(req,res)=>res.sendFile(path.join(__dirname,'views','hamilton.html')))
+app.get('/hamilton',(req,res)=>res.sendFile(path.join(__dirname,'views','hamilton.html')))
 app.get('/hopper',(req,res)=>res.sendFile(path.join(__dirname,'views','hopper.html')))
 app.get('/lovelace',(req,res)=>res.sendFile(path.join(__dirname,'views','lovelace.html')))
 app.get('/turing',(req,res)=>res.sendFile(path.join(__dirname,'views','turing.html')))
